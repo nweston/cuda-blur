1ff7c00dcfd642774c3a71666eb3df2264b1648b
Nathan Weston <nweston@fastmail.com>
2020-11-13 09:31:16 -0500

    Average multiple runs in timeit() function.

--- timer.h
+++ timer.h
@@ -10,13 +10,18 @@
 template <class T>
 void timeit(const std::string &name, T function,
             std::ostream &out = std::cout) {
-  auto start = std::chrono::high_resolution_clock::now();
-  function();
-  cudaDeviceSynchronize();
-  auto end = std::chrono::high_resolution_clock::now();
-  // Duration in miliseconds
-  auto elapsed =
-      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  const int RUNS = 5;
+  auto total_time = std::chrono::milliseconds(0);
 
-  out << name << ": " << elapsed.count() << " ms\n";
+  for (int i = 0; i < RUNS; i++) {
+    auto start = std::chrono::high_resolution_clock::now();
+    function();
+    cudaDeviceSynchronize();
+    auto end = std::chrono::high_resolution_clock::now();
+    // Duration in miliseconds
+    total_time +=
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  }
+
+  out << name << ": " << total_time.count() / float(RUNS) << " ms\n";
 }
