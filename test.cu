d75121f6d13545f1961d5616df2e7c40e74e0835
Nathan Weston <nweston@fastmail.com>
2020-12-01 12:47:53 -0500

    Add automated correctness tests.

--- test.cu
+++ test.cu
@@ -1,6 +1,7 @@
 #include "cuda_half.h"
 
 #include <nppi_filtering_functions.h>
+#include <algorithm>
 #include <memory>
 #include <string>
 #include <utility>
@@ -80,20 +81,112 @@ static void npp_blur(float4* dest, const float4* source, float4* temp,
   // FIXME: this will only work for odd numbers of passes
 }
 
+static float diff(const float4& a, const float4& b) {
+  return std::abs(std::max({a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w}));
+}
+
+int run_checks(float4* pixels, image_dims dims) {
+  auto source = alloc_and_copy(pixels, dims);
+  auto dest = cuda_malloc_unique<float4>(allocated_bytes(dims));
+  auto temp = cuda_malloc_unique<float4>(allocated_bytes(dims));
+
+  std::vector<int> radii{1, 3, 5, 10, 20, 50, 100};
+  auto baseline = std::make_unique<float4[]>(allocated_pixels(dims));
+  auto result = std::make_unique<float4[]>(allocated_pixels(dims));
+
+  std::vector<std::string> failures;
+
+  const int n_passes = 3;
+  for (int crop = 0; crop <= 1; crop++) {
+    if (crop) {
+      crop_image(pixels, dims);
+    }
+
+    for (auto radius : radii) {
+      auto check_result = [&](const std::string& name,
+                              float threshold = 0.0001) {
+        copy_image(result.get(), dest.get(), dims);
+        float max_diff = 0;
+        for (size_t y = 0; y < dims.height; y++) {
+          for (size_t x = 0; x < dims.width; x++) {
+            auto index = pixel_index(dims, x, y);
+            max_diff = std::max(
+                max_diff, diff(baseline.get()[index], result.get()[index]));
+          }
+        }
+        if (max_diff > threshold) {
+          std::cout << "x" << std::flush;
+          failures.push_back(name + " radius " + std::to_string(radius) +
+                             (crop ? " (cropped)" : " ") +
+                             " failed: max diff " + std::to_string(max_diff));
+        } else {
+          std::cout << "." << std::flush;
+        }
+      };
+
+      // Use iterated box blur, one thread per column, as the baseline.
+      smooth_blur(dest.get(), source.get(), temp.get(), dims, radius, n_passes);
+      copy_image(baseline.get(), dest.get(), dims);
+
+      // Multiple outputs per thread
+      for (int outputs_v = 2; outputs_v <= 3; outputs_v++) {
+        for (int outputs_h = 2; outputs_h <= 3; outputs_h++) {
+          smooth_blur(dest.get(), source.get(), temp.get(), dims, radius,
+                      n_passes, outputs_v, outputs_h, 1, 1);
+          check_result("outputs " + std::to_string(outputs_v) +
+                       std::to_string(outputs_h));
+        }
+      }
+
+      // Column splitting
+      for (int threads_v = 2; threads_v <= 4; threads_v++) {
+        for (int threads_h = 2; threads_h <= 4; threads_h++) {
+          smooth_blur(dest.get(), source.get(), temp.get(), dims, radius,
+                      n_passes, 1, 1, threads_v, threads_h);
+          check_result("columns " + std::to_string(threads_v) +
+                       std::to_string(threads_h));
+        }
+      }
+
+      // Direct box filter
+      direct_blur_no_transpose(dest.get(), source.get(), temp.get(), dims,
+                               radius, n_passes, 2);
+      check_result("direct");
+
+      // Direct Gaussian blur
+      if (radius <= MAX_PRECOMPUTED_RADIUS) {
+        precomputed_gaussian_blur(dest.get(), source.get(), temp.get(), dims,
+                                  radius, 2);
+        // Large threshold for now, due to edge artifacts
+        check_result("gaussian", 0.03f);
+      }
+    }
+  }
+  std::cout << "\n";
+
+  if (failures.empty()) {
+    std::cout << "OK!\n";
+    return 0;
+  } else {
+    for (auto& n : failures) {
+      std::cout << n << "\n";
+    }
+    return failures.size();
+  }
+}
+
 int main(int argc, char** argv) {
-  bool do_crop = false;
   bool do_outputs = false;
   bool do_column_split = false;
   bool do_npp = false;
   bool do_direct = false;
   bool do_gaussian = false;
+  bool do_check = false;
 
   std::vector<std::string> args;
   for (int i = 1; i < argc; i++) {
     std::string a(argv[i]);
-    if (a == "-crop")
-      do_crop = true;
-    else if (a == "-outputs")
+    if (a == "-outputs")
       do_outputs = true;
     else if (a == "-columns")
       do_column_split = true;
@@ -103,6 +196,8 @@ int main(int argc, char** argv) {
       do_direct = true;
     else if (a == "-gaussian")
       do_gaussian = true;
+    else if (a == "-check")
+      do_check = true;
     else
       args.push_back(a);
   }
@@ -114,8 +209,9 @@ int main(int argc, char** argv) {
   // Work around stupid "structured binding can't be captured" issue
   auto dims = _dims;
 
-  if (do_crop)
-    crop_image(pixels.get(), dims);
+  if (do_check) {
+    return run_checks(pixels.get(), dims);
+  }
 
   auto source = alloc_and_copy(pixels.get(), dims);
   auto dest = cuda_malloc_unique<float4>(allocated_bytes(dims));
